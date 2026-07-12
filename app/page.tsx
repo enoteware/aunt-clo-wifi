e94580ef0f3e3ba907f2818169004fa4420acd44
Elliot <enoteware@gmail.com>
2026-07-12 17:39:12 +0000

    Simplify to static Markdown + GitHub Pages

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "system-ui" }}>
-      <h1>Aunt Clo WiFi Advice</h1>
-      <p>Updated troubleshooting guidance will appear here.</p>
-    </main>
-  );
-}
